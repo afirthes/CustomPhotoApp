a8d5795bac3e4c38f186bb673794849dd7bb176e
Afir Thes <afirthes@gmail.com>
2021-09-10 08:13:39 +0300

    UI and camera session setup

---
+++ CustomPhotoApp/CapturedImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CapturedImageViewController.swift
+//  CustomPhotoApp
+//
+//  Created by Afir Thes on 10.09.2021.
+//
+
+import UIKit
+
+class CapturedImageViewController: UIViewController {
+
+    @IBOutlet weak var capturedImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func dismissButtonAction(_ sender: Any) {
+    }
+    
+
+}
--- CustomPhotoApp/ViewController.swift
+++ CustomPhotoApp/ViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var camPreview: UIView!
+    @IBOutlet weak var captureButton: UIButton!
+    @IBOutlet weak var capturedImageThumbnail: UIButton!
+    
+    let captureSession = AVCaptureSession()
+    
+    var photoOutput = AVCapturePhotoOutput()
+    
+    var activeInput: AVCaptureDeviceInput!
+    
+    var isSessionSetup = false
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        captureButton.layer.cornerRadius = captureButton.layer.frame.width / 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func startSession() {
+        DispatchQueue.main.async {
+            self.captureSession.startRunning()
+        }
+    }
+    
+    func stopSession()  {
+        DispatchQueue.main.async {
+            self.captureSession.stopRunning()
+        }
+    }
+    
+    func setupSession() -> Bool {
+        
+        captureSession.beginConfiguration() // atomically
+        
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo // high - for video
+        
+        // looking for default capture device for video
+        let camera = AVCaptureDevice.default(for: AVMediaType.video)!
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: camera)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+                activeInput = input
+            } else {
+                print("Was not able to add input device!")
+                captureSession.commitConfiguration()
+                return false
+            }
+        } catch  {
+            print("Was not able to add input device!")
+            captureSession.commitConfiguration()
+            return false
+        }
+        
+        if captureSession.canAddOutput(photoOutput) {
+            captureSession.addOutput(photoOutput)
+            photoOutput.isHighResolutionCaptureEnabled = true
+        } else {
+            print("Was not able to create photo output!")
+            captureSession.commitConfiguration()
+            return false
+        }
+        
+        captureSession.commitConfiguration()
+        
+        self.isSessionSetup = true
+        
+        return true
     }
 
-
+    @IBAction func capture(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showCapturedImage(_ sender: Any) {
+    }
+    
 }
 
